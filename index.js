120d23c9c915c378dff1ff3c74ad0fce484e2b57
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-05-01 13:02:04 +0300

    task6

--- index.js
+++ index.js
@@ -196,6 +196,11 @@
 
 let input;
 let total = 0;
+do { 
+input = prompt("Введите число : ");
+total = total + Number(input)
+} while(input !== null)
+alert(`Общая сумма чисел равна ${total}`)
 
 
 
