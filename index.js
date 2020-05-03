7f03cdbeb22ac95d4ccacb3ab18b0f1413b2647f
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-05-04 01:58:26 +0300

    fix

--- index.js
+++ index.js
@@ -354,33 +354,69 @@
 // В prototype функции-конструктора добавь метод getInfo(),
 // который выводит в консоль значения полей login и email объекта который его вызвал.
 
-const Account = function({login, email}) {
-  this.login = login;
-  this.email = email;
+// const Account = function({login, email}) {
+//   this.login = login;
+//   this.email = email;
   
-};
+// };
 
-    Account.prototype.getInfo = function() {
-      console.log(`Login:${this.login}, Email:${this.email}`);
-    };
+//     Account.prototype.getInfo = function() {
+//       console.log(`Login:${this.login}, Email:${this.email}`);
+//     };
 
-console.log(Account.prototype.getInfo); // function
+// console.log(Account.prototype.getInfo); // function
 
-const mango = new Account({
-  login: 'Mangozedog',
-  email: 'mango@dog.woof',
+// const mango = new Account({
+//   login: 'Mangozedog',
+//   email: 'mango@dog.woof',
+// });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+
+//============================================== #13 ========================================================
+
+
+// Напиши класс User для создания пользователя со следующим свойствами:
+// name - строка
+// age - число
+// followers - число
+// Добавь метод getInfo(), который, выводит строку:
+// User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+
+
+class User {
+  constructor({name, age, followers}) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    );
+  }
+}
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
 });
-
-mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-
-const poly = new Account({
-  login: 'Poly',
-  email: 'poly@mail.com',
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
 });
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
 
-poly.getInfo(); // Login: Poly, Email: poly@mail.com
 
-
-//============================================== #1 ========================================================
 //============================================== #1 ========================================================
 //============================================== #1 ========================================================
