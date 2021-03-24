8ecbb6cb724e0df7296fcfeecf6edfa538e7874d
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-03-24 03:34:32 +0200

    1.3

--- index2.js
+++ index2.js
@@ -9,17 +9,33 @@
 //?     - genres - сюда поместить пустой массив
 //?     - privat - в это свойство поместить booleam(логическое) значение false
 
-const massageOne = prompt('Сколько фильмов вы уже посмотрели') 
-let numberOfLilms = massageOne;
+//? Задайте пользователю по два раза вопрос :
+//?     - 'Один из последних просмотренных фильмов?'
+//?     - 'На сколько оцените его?'
+//? Ответы стоит поместить в отдельные переменные
+//? Записать ответы в объект movies в формате :
+//? movies:{
+//?     'logan': '8.1'
+//? }
+//? Проверить что бы все работало без ошибок в консоли
+
+const numberOfLilms = +prompt('Сколько фильмов вы уже посмотрели', '')
+const nameFilm1 = prompt('Один из последних просмотренных фильмов?','')
+const rating1 = prompt('На сколько оцените его?','')
+const nameFilm2 = prompt('Один из последних просмотренных фильмов?','')
+const rating2 = prompt('На сколько оцените его?','') 
 
 const personalMovieBD = {
-    'count': numberOfLilms,
-    'movies': {},
-    'actors': {},
-    'genres': [],
-    'privat': false,
+     count  : numberOfLilms,
+     movies : {},
+     actors : {},
+     genres : [],
+     privat : false,
 
 }
+personalMovieBD.movies[nameFilm1] = rating1;
+personalMovieBD.movies[nameFilm2] = rating2;
+
 console.log('personalMovieBD :>> ', personalMovieBD);
 
 
