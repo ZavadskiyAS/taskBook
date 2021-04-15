b044a2a54b2412ddc416c209731a07602d4051bb
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-04-15 23:00:03 +0300

    fix task new branch

--- index2.js
+++ index2.js
@@ -335,70 +335,70 @@
 //? Напишите функцию, которая пишет в консоль число в заданном диапазоне,
 //? в случае, если оно успешно делится или не делится с остатком или без остатка в зависимости от параметров.
 
-function getNumbersModulatordBy(modulus, loggerCallback) {
+// function getNumbersModulatordBy(modulus, loggerCallback) {
   // Функция, которая возвращает функцию - это подход из функционального программирования
   // Называется замыканием (Closure)
-  return function(start, end) {
-    loggerCallback({ message: "Конфигурация", config: configuration });
-    loggerCallback({ message: "Полученный модулятор", modulus: modulus });
-    loggerCallback({
-      message: "Полученный start и end",
-      start: start,
-      end: end
-    });
+  // return function(start, end) {
+  //   loggerCallback({ message: "Конфигурация", config: configuration });
+  //   loggerCallback({ message: "Полученный модулятор", modulus: modulus });
+  //   loggerCallback({
+  //     message: "Полученный start и end",
+  //     start: start,
+  //     end: end
+  //   });
   
-    while (start <= end) {
+    // while (start <= end) {
       // Стоит обратить внимание:
       // 1. При нестрогом равенстве true == 1 и false == 0
       // 2. Здесь мы обращаемся к глобальному контексту configuration
       // p.s. поле isEntry может меняться
-      if (start % modulus == configuration.isEntry) {
+      // if (start % modulus == configuration.isEntry) {
         // В данном случае мы используем loggerCallback как middleware
         // Это определит дальнейшую судьбу результата
         // Позволяет убрать side-effect
-        loggerCallback(start);
-      }
-      start++;
-    }
-  };
- }
+        // loggerCallback(start);
+//       }
+//       start++;
+//     }
+//   };
+//  }
   
  // Глобальный контекст не имеет блочной видимости
  // В данном случае переменная поднимается выше в самое начало кода
  // Исполнитель JavaScript видит её в независимости от места инициализации
- var configuration = {
-  modulus: 10,
-  isEntry: false,
-  start: 45,
-  end: 68
- };
+//  var configuration = {
+//   modulus: 10,
+//   isEntry: false,
+//   start: 45,
+//   end: 68
+//  };
   
- var loggerCallback = data => console.log(data);
+//  var loggerCallback = data => console.log(data);
   
- let tenNumbersModulator = getNumbersModulatordBy(
-  configuration.modulus,
-  loggerCallback
- );
+//  let tenNumbersModulator = getNumbersModulatordBy(
+//   configuration.modulus,
+//   loggerCallback
+//  );
   
  // Переменные с глобальным контекстом доступны из:
  // 1. globalThis (в Node.js)
  // 2. window (в браузере)
- window.configuration.modulus = 5;
+//  window.configuration.modulus = 5;
   
- let fiveNumbersModulator = getNumbersModulatordBy(
-  configuration.modulus,
-  loggerCallback
- );
+//  let fiveNumbersModulator = getNumbersModulatordBy(
+//   configuration.modulus,
+//   loggerCallback
+//  );
   
- tenNumbersModulator(configuration.start, configuration.end); // 50, 60
- tenNumbersModulator(10, 100); // 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+//  tenNumbersModulator(configuration.start, configuration.end); // 50, 60
+//  tenNumbersModulator(10, 100); // 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
   
  // Так как мы изменили поле isEntry, то теперь:
- // Функция вернёт те значения, которые не входят в модуляцию числа
- window.configuration.isEntry = true;
+//  // Функция вернёт те значения, которые не входят в модуляцию числа
+//  window.configuration.isEntry = true;
   
- fiveNumbersModulator(configuration.start, configuration.end); // 46, 51, 56, 61, 66
- fiveNumbersModulator(10, 21); // 11, 16, 21
+//  fiveNumbersModulator(configuration.start, configuration.end); // 46, 51, 56, 61, 66
+//  fiveNumbersModulator(10, 21); // 11, 16, 21
 //!============================================== #9 ========================================================
 //!============================================== #10 =======================================================
 //?  Какое будет выведено значение: let x = 5; alert( x++ ); ?
